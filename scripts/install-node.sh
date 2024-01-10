5adcc38ae4127ab8873c6c052746864c4ecbcade
MBA Yacine <yacine@MBP-de-MBA.lan>
2024-01-10 22:23:23 +0100

    feat: automated s3 deployment

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -1,10 +1,6 @@
-wget -nv https://d3rnber7ry90et.cloudfront.net/linux-x86_64/node-v18.17.1.tar.gz
-mkdir /usr/local/lib/node
-tar -xf node-v18.17.1.tar.gz
-mv node-v18.17.1 /usr/local/lib/node/nodejs
-### Unload NVM, use the new node in the path, then install some items globally.
-echo "export NVM_DIR=''" >> ~/.bashrc
-echo "export NODEJS_HOME=/usr/local/lib/node/nodejs" >> ~/.bashrc
-echo "export PATH=\$NODEJS_HOME/bin:\$PATH" >> ~/.bashrc
-### Reload environment
-. ~/.bashrc
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+nvm install node
+n=$(which node)
+n=${n%/bin/node}
+chmod -R 755 $n/bin/* 
+sudo cp -r $n/{bin,lib,share} /usr/local 
