3f837df62f58f72a0be173a7d8899a2c1cda7784
MBA Yacine <yacine@MBP-de-MBA.lan>
2024-01-16 22:30:39 +0100

    fix: renamed homepage title

--- app/page.tsx
+++ app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
 
   return (
     <div>
-      <h1>Homepage displaying all of our products</h1>
+      <h1>Homepage</h1>
       <div className="flex">
         {articles?.map((article) => (
           <CCard style={{ width: "18rem" }} key={article.name}>
